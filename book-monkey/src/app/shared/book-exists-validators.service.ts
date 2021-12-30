7627778c3e5db38bed70821aa777fb6da7c8480e
Achim <a.grether@hotmail.com>
2021-12-30 23:20:31 +0100

    changed input valitation with abstractcontrols

--- book-monkey/src/app/shared/book-exists-validators.service.ts
+++ book-monkey/src/app/shared/book-exists-validators.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { AbstractControl, AsyncValidator, ValidationErrors } from '@angular/forms';
+import { catchError, map, Observable, of } from 'rxjs';
+import { BookStoreService } from './book-store.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class BookExistsValidatorsService {
+export class BookExistsValidatorsService implements AsyncValidator {
 
-  constructor() { }
+  constructor(private bs: BookStoreService) { }
+
+    validate(control: AbstractControl): Observable<ValidationErrors | null> {
+      return this.bs.check(control.value).pipe(
+        map(exists => (exists === false) ? null : {
+          isbnExists: { valid: false }
+        }),
+        catchError(() => of(null))
+      );
+    }
 }
--- book-monkey/src/app/shared/book-validators.ts
+++ book-monkey/src/app/shared/book-validators.ts
@@ -1,2 +1,28 @@
+import { ValidationErrors, AbstractControl, FormArray } from "@angular/forms";
+
 export class BookValidators {
+    static isbnFormat(control: AbstractControl): ValidationErrors | null {
+        if(!control.value) { return null; }
+
+        const numbers = control.value.replace(/-/g, '');
+        const isbnPattern = /(^\d{10}$) | (^\d{13}$)/;
+
+        if (isbnPattern.test(numbers)) {
+            return null;
+        } else {
+            return {
+                isbnFormat: { valid: false }
+            };
+        }
+    }
+
+    static atLeastOneAuthor(controlArray: AbstractControl): ValidationErrors | null {
+        if ((controlArray as FormArray).controls.some((el: AbstractControl) => el.value)) {
+            return null;
+        } else {
+            return {
+                atLeastOneAuthor: { valid: false }
+            };
+        }
+    }
 }
