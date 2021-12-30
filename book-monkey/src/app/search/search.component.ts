8e5ec2f0a07e4888deccddf5aa0728f1119d3ca1
Achim <a.grether@hotmail.com>
2021-12-30 10:36:00 +0100

    add implementation of book-search

--- book-monkey/src/app/search/search.component.ts
+++ book-monkey/src/app/search/search.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { debounceTime, distinctUntilChanged, filter, Subject, switchMap } from 'rxjs';
+import { Book } from '../shared/book';
+import { BookStoreService } from '../shared/book-store.service';
 
 @Component({
   selector: 'bm-search',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+  foundBooks: Book[] = [];
+  keyUp$ = new Subject<string>();
 
-  constructor() { }
+  constructor(private bs:BookStoreService) { }
 
   ngOnInit(): void {
+    this.keyUp$.pipe(
+      filter(term => term.length >= 3),
+      debounceTime(500),
+      distinctUntilChanged(),
+      switchMap(searchTerm => this.bs.getAllSearch(searchTerm))
+    ).subscribe(books => this.foundBooks = books);
   }
 
 }
